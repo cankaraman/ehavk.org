3d01d391dd5dc0c7555a532ac3e31772b6cfc6ad
can karaman <ycankaraman@gmail.com>
2020-03-05 23:39:45 +0300

    add photo grid page

---
+++ js/base-page.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // Get the current year for the copyright
+    $('#year').text(new Date().getFullYear());
+    // tooltip init
+    $('[data-toggle="tooltip"]').tooltip();
+})
+
+$(".card-btn").click(function(e){
+    var cardOpenClass = "fas fa-minus ml-auto"
+    var cardClosedClass = "fas fa-plus ml-auto"
+    var currentClass = $(this).find("i").attr("class")
+    if (currentClass == cardOpenClass){
+        $(this).find("i").attr("class", cardClosedClass)
+    } else {
+        $(this).find("i").attr("class", cardOpenClass)
+    }
+});
+
+
